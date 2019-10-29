d57deeda0f127094bb36c3e6004676db9d18e367
cristian.cdi@gmail.com <Neverwinter2>
2019-10-28 23:03:27 -0300

    Se trabaja en los templates de mail y se agrega posibilidad de preview

---
+++ emails/test/@preview-test.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const Email = require('email-templates');
+
+const email = new Email({
+    message: {
+        from: process.env.APP_NAME + "<" + process.env.SMTP_USER + ">",
+        to: "yo",
+    },
+    send: false,
+    preview: true
+});
+
+email.send({
+    template: 'test',
+    message: {
+        from: process.env.APP_NAME + "<" + process.env.SMTP_USER + ">",
+        to: "me",
+    },
+    locals: {
+        appName: process.env.APP_NAME,
+        name:  "NAME",
+        url: "URL",
+        username: "USERNAME"
+    }
+}).then(console.log)
+    .catch(console.error);
