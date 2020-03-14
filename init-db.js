46e64bb14782c0442c8802cfc8e9b212f3529d70
cincarnato <cristian.incarnato@sondeos.com.ar>
2020-03-14 12:11:05 -0300

    Se agrega init de security

---
+++ init-db.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import mongoose from 'mongoose'
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+
+const { ObjectId } = mongoose.Types;
+ObjectId.prototype.valueOf = function () {
+    return this.toString();
+};
+
+import {initSecurity} from './init/init-security'
+
+initSecurity()
