d7ea502088b8641fc96d2b95a84ab4c0550c849c
cincarnato <cristian.incarnato@sondeos.com.ar>
2020-04-15 12:31:10 -0300

    Fix IPv6

--- src/modules/security/services/SessionService.js
+++ src/modules/security/services/SessionService.js
@@ -22,6 +22,9 @@ function getGeo(ip) {
 
 function getIpv4(ip){
    // let regExp = new RegExp(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/)
+    if(ip === '::1'){
+        return '127.0.0.1'
+    }
     return ip.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)[0]
 }
 
